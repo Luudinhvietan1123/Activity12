b9e1ccbb3ef9a62d37e92775da5aebfe5048269d
việt Ân <luudinhvietan1123@gmail.com>
2020-11-25 14:31:31 +0700

    fresh

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        MyThread t1 = new MyThread("ABC", 3500);
+        MyThread t2 = new MyThread("DEF", 2500);
+    }
+}
---
+++ src/MyThread.java
@@ -0,0 +1,36 @@
+public class MyThread implements Runnable{
+    //Nên sử dụng implement runnable để tránh tình trạng đa kế thừa khi kế thừa thread
+
+    public Thread t;
+    public String name;
+    public int sleep;
+
+    public MyThread(String threadName, int timeSleep) throws InterruptedException {
+        name = threadName;
+        t = new Thread(this, name);
+        t.sleep(timeSleep);
+        t.start();
+    }
+
+    //How to handle interrupted exception in java thread.sleep without swallow it?
+
+    /*
+    -->
+    public MyThread(String threadNAme, int timeSleep){
+        name = threadName;
+        t = new Thread(this, name);
+        try{
+            while(!Thread.currentThread().isInterrupted()){
+                t1.sleep(timeSleep);
+            }
+        }catch(InterruptedException e){
+            t1.currentThread().interrupt();
+        }
+    }
+    */
+
+
+    public void run(){
+        System.out.println(name);
+    }
+}
